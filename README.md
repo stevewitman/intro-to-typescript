970c1e3c92b8f47d3d0808c64616ed17ea594bfb
Steve Witman <stevewitman@gmail.com>
2017-06-12 11:47:17 -0600

    Add link to slides

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Presentation by Steve Witman
 ### Angular Boulder Meetup (June 12, 2017)
+
+"Slides" can be found in the [template for app.component](https://github.com/stevewitman/intro-to-typescript/blob/master/src/app/app.component.html) 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  code01types = `
-      class Item {
-
-        constructor(
-            public cost: number,
-            public name: string,
-            public desc: string) {
-        }  
-
-        function getDetails() {
-            return 'Name:$ (this.name)
-                Price: $(this.cost)
-            '
-        }
-    }
-  `
-
 
 }
